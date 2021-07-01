32b957d9fc6f62782b5dbcd10c6ba8cf2d2147e0
agmcc <agmcc@users.noreply.github.com>
2021-07-01 20:21:00 +0100

    Add single pod k3s tests

--- k3s/create_secret.sh
+++ k3s/create_secret.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Needed for private registry
+if [ -n "$1" ]
+then
+    namespace=$1
+else
+    namespace=default
+fi
+
+echo "Creating secret for namespace: ${namespace}"
+
 read -p "Enter GitHub container registry PAT: " password
 
 sudo kubectl create secret docker-registry ghcr \
 --docker-server=ghcr.io \
 --docker-username=agmcc \
---docker-password=$password
+--docker-password=$password \
+-n $namespace
